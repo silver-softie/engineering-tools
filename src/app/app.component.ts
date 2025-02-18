2a76a618ca6d5f6424aca9c70cf291be564772dc
Brad Moffat <brad@moffat.me.uk>
2025-02-18 20:16:08 +0000

    Almost dynamic components but still broken.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, ComponentRef, OnInit, ComponentFactoryResolver, TemplateRef, Renderer2 } from '@angular/core';
 import { PasswordGeneratorComponent } from './password-generator/password-generator.component';
 import { GuidGeneratorComponent } from './guid-generator/guid-generator.component';
 import { UrlEncoderComponent } from './url-encoder/url-encoder.component';
+
+interface DynamicComponent {
+  component: any,
+  headingId: string,
+  collapse: string,
+  heading: string
+}
 
 @Component({
   selector: 'app-root',
@@ -15,4 +22,54 @@ import { UrlEncoderComponent } from './url-encoder/url-encoder.component';
 })
 export class AppComponent {
   title = 'engineering-tools';
+
+  @ViewChild('viewContainerRef', { read: ViewContainerRef, static: true }) viewContainerRef!: ViewContainerRef;
+
+  components: DynamicComponent[] = [
+    { component: GuidGeneratorComponent, headingId: 'headingOne', collapse: 'collapseOne', heading: 'GUID Generator' },
+    //{ component: PasswordGeneratorComponent, headingId: 'headingTwo', collapse: 'collapseTwo', heading: 'Password Generator' },
+    //{ component: UrlEncoderComponent, headingId: 'headingThree', collapse: 'collapseThree', heading: 'URL Encoder' }
+  ];
+
+  beforeTemplate = `
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingOne">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+          aria-expanded="true" aria-controls="collapseOne">
+          GUID Generator
+        </button>
+      </h2>
+      <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+        data-bs-parent="#toolAccordion">
+        <div class="accordion-body">`;
+
+  afterTemplate = `</div></div></div>`;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver, private renderer: Renderer2) {
+  }
+
+  ngOnInit(): void {
+    this.viewContainerRef.clear();
+    this.components.forEach(dynamicComponent => {
+
+      // Add markup before the component
+      const beforeElement = this.renderer.createElement('div');
+      beforeElement.innerHTML = this.beforeTemplate;
+      this.viewContainerRef.element.nativeElement.parentElement.appendChild(beforeElement);
+
+      const componentFactory = this.componentFactoryResolver.resolveComponentFactory(dynamicComponent.component);
+      this.viewContainerRef.createComponent(componentFactory);
+
+      //this.viewContainerRef.element.nativeElement.parentElement.appendChild(componentFactory.create(dynamicComponent.component));
+
+      // Add markup after the component
+      const afterElement = this.renderer.createElement('div');
+      afterElement.innerHTML = this.afterTemplate;
+      this.viewContainerRef.element.nativeElement.parentElement.appendChild(afterElement);
+    });
+
+  }
+
+
 }
+
