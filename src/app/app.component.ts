e8a07ef49c1a6ebb09dbe6e99e987c2d37aa592a
Brad Moffat <brad@moffat.me.uk>
2025-03-05 19:41:05 +0000

    Fixed type in great-circle-distance.component.ts. Removed GreatCircleComponent from app.component.ts Removed unused RouterOutlet from app.component.ts Removed unused type DynamicComponent from app.component.ts Updated version in package.json to 0.0.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { BannerComponent } from './banner/banner.component';
 import { FooterComponent } from './footer/footer.component';
-import { GreatCircleDistanceComponent } from './great-circle-distance/great-circle-distance.component';
-
-interface DynamicComponent {
-  component: any,
-  headingId: string,
-  collapse: string,
-  heading: string
-}
 
 @Component({
   selector: 'app-root',
   imports: [
     BannerComponent,
     FooterComponent,
-    GreatCircleDistanceComponent,
     RouterModule,
-    RouterOutlet,
     HomeComponent,
   ],
   templateUrl: './app.component.html',
