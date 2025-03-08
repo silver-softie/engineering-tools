ad26408a46abc9563ad1ffe55ffcc10fe926ed3a
Sortient <37266971+Sortient@users.noreply.github.com>
2025-03-08 16:09:56 +0000

    Updated home page to use an Angular Material sidenav instead of a mat-accordion.
    Tried to group tools together too but selecting them this way was a bit trickier than just listing them each as their own nav list item.
    
    Can't seem to fix an issue where a tool isn't rendered until a nav-item is actually clicked though.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,50 +1,54 @@
-import { Component, ComponentFactoryResolver, QueryList, ViewChildren, ViewContainerRef } from '@angular/core';
+import { Component, ComponentFactoryResolver, QueryList, ViewChildren, ViewContainerRef, ViewChild, AfterViewInit } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ToolRegistryService } from '../services/tool-registry.service';
-
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, MatExpansionModule, HttpClientModule],
+  imports: [CommonModule, MatExpansionModule, HttpClientModule, MatSidenavModule, MatListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  firstToolSelected: boolean = false;
   tools: Array<{ name: string, component: any }> = [];
-
-  @ViewChildren('viewContainer', { read: ViewContainerRef }) viewContainers!: QueryList<ViewContainerRef>;
+  selectedTool: { name: string, component: any } | null = null;
+  
+  @ViewChild('toolContainer', { read: ViewContainerRef, static: true }) toolContainer!: ViewContainerRef;
 
   constructor(
     private componentFactoryResolver: ComponentFactoryResolver,
     private http: HttpClient,
     private toolRegistry: ToolRegistryService
-  ) {
-  }
+  ) { }
 
   ngOnInit() {
     this.http.get<{ tools: any[] }>('config/all-tools.json').subscribe(config => {
       this.toolRegistry.getComponentsFromConfig(config.tools).then(toolComponents => {
-          this.tools = toolComponents;
-          this.viewContainers.forEach((viewContainer, index) => {
-            this.insertTool(this.tools[index], viewContainer);
-          });
-        });
-    });
-  }
-
-  insertTool(tool: any, viewContainer: ViewContainerRef): boolean {
-    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(tool.component);
-    viewContainer.clear();
-    viewContainer.createComponent(componentFactory);
-    return true;
-  }
-
-  ngAfterViewInit() {
-    this.viewContainers.changes.subscribe(() => {
-      this.viewContainers.forEach((viewContainer, index) => {
-          this.insertTool(this.tools[index], viewContainer);
+        this.tools = toolComponents;
       });
     });
+    this.selectTool(this.tools[0]);
+  }
+
+  ngAfterViewInit(): void {
+    this.loadSelectedTool();
+  }
+
+  selectTool(tool: { name: string, component: any }) {
+    this.selectedTool = tool;
+    this.loadSelectedTool();
+  }
+
+  loadSelectedTool() {
+    if (this.selectedTool && this.toolContainer) {
+      setTimeout(() => {
+        const componentFactory = this.componentFactoryResolver.resolveComponentFactory(this.selectedTool!.component);
+        this.toolContainer.clear();
+        this.toolContainer.createComponent(componentFactory);
+      });
+    }
   }
 }
